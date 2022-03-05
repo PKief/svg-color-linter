b253d184530730014d903aba59546343c6df69c9
Philipp Kief <philipp.kief@gmx.de>
2022-03-05 23:15:47 +0100

    Support glob file patterns for CLI (#1)
    * Support glob file patterns for CLI
    
    * Improve performance by using is-glob package

--- src/core/async.ts
+++ src/core/async.ts
@@ -1,5 +1,6 @@
 import { exec } from 'child_process';
 import { mkdir, readFile, writeFile } from 'fs';
+import glob from 'glob';
 import { dirname } from 'path';
 import { promisify } from 'util';
 
@@ -7,10 +8,11 @@ const execAsync = promisify(exec);
 const writeFileAsync = promisify(writeFile);
 const readFileAsync = promisify(readFile);
 const mkdirAsync = promisify(mkdir);
+const globAsync = promisify(glob);
 
 const outputFileAsync = async (path: string, contents: string) => {
   await mkdirAsync(dirname(path), { recursive: true });
   await writeFileAsync(path, contents);
 };
 
-export { execAsync, writeFileAsync, readFileAsync, outputFileAsync };
+export { execAsync, writeFileAsync, readFileAsync, outputFileAsync, globAsync };
